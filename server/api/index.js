b998e81dfaaddd1d289bb06541f9de8d46e0349c
Harsh Jakhar <jakharharsh369@gmail.com>
2025-12-08 01:39:57 +0530

    fix: runtime

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ import app from "../server.js";
 import serverless from "serverless-http";
 
 export const config = {
-  runtime: "nodejs20.x",
+  runtime: "nodejs",
 };
 
 export default serverless(app);
